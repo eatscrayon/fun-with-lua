5fa3a4f5a10f0a80be3c986d5d1200723c78764f
eatscrayon <99401683+eatscrayon@users.noreply.github.com>
2022-10-06 20:54:58 -0400

    Update nes_game_genie.lua

--- nes_game_genie.lua
+++ nes_game_genie.lua
@@ -1,41 +1,14 @@
 local grimoire = {
-	['A']=  0x0,	
-	['P']=  0x1,
-	['Z']=  0x2,
-	['L']=  0x3,
-	['G']=  0x4,
-	['I']=  0x5,
-	['T']=  0x6,
-	['Y']=  0x7,
-	['E']=  0x8,
-	['O']=  0x9,
-	['X']=  0xA,
-	['U']=  0xB,
-	['K']=  0xC,
-	['S']=  0xD,
-	['V']=  0xE,
-	['N']=  0xF,
-	[0x0]=  'A',
-	[0x1]=  'P',
-	[0x2]=  'Z',
-	[0x3]=  'L',
-	[0x4]=  'G',
-	[0x5]=  'I',
-	[0x6]=  'T',
-	[0x7]=  'Y',
-	[0x8]=  'E',
-	[0x9]=  'O',
-	[0xA]=  'X',
-	[0xB]=  'U',
-	[0xC]=  'K',
-	[0xD]=  'S',
-	[0xE]=  'V',
-	[0xF]=  'N',
+	['A']=  0x0,['P']=  0x1,['Z']=  0x2,['L']=  0x3,['G']=  0x4,['I']=  0x5,['T']=  0x6,['Y']=  0x7,
+	['E']=  0x8,['O']=  0x9,['X']=  0xA,['U']=  0xB,['K']=  0xC,['S']=  0xD,['V']=  0xE,['N']=  0xF,
+	[0x0]=  'A',[0x1]=  'P',[0x2]=  'Z',[0x3]=  'L',[0x4]=  'G',[0x5]=  'I',[0x6]=  'T',[0x7]=  'Y',
+	[0x8]=  'E',[0x9]=  'O',[0xA]=  'X',[0xB]=  'U',[0xC]=  'K',[0xD]=  'S',[0xE]=  'V',[0xF]=  'N',
 }
 
 local code_to_patch = function (inputcode)
     local hexcode = {}
     local out_patch_data = {["address"]= nil,["value"] = nil, ["compare"]= nil }
+
     if string.len(inputcode) == 6 then
         for index = 1, #inputcode do
             table.insert(hexcode,grimoire[string.char(inputcode:byte(index))])
@@ -45,7 +18,7 @@ local code_to_patch = function (inputcode)
         local g_value = ((hexcode[2] & 7) << 4) | ((hexcode[1] & 8) << 4) | (hexcode[1] & 7) | (hexcode[6] & 8)
         out_patch_data["value"] = g_value
         out_patch_data["starting_value"] = g_value
-    
+
     elseif string.len(inputcode) == 8 then
         for index = 1, #inputcode do
             table.insert(hexcode,grimoire[string.char(inputcode:byte(index))])
@@ -100,7 +73,6 @@ local patch_to_code = function (in_patch_data)
 		local Fggg = (in_patch_data["address"] & 15)
 		local Abbb = (in_patch_data["value"] >> 4)
 		local Haaa = (in_patch_data["value"] & 15)
-
 		local E = Efff & 8
 		local B = Bccc & 8
 		local D = Deee & 8
@@ -108,7 +80,6 @@ local patch_to_code = function (in_patch_data)
 		local F = Fggg & 8
 		local A = Abbb & 8
 		local H = Haaa & 8
-
 		local ddd = Xddd & 7
 		local fff = Efff & 7
 		local ccc = Bccc & 7
@@ -117,7 +88,6 @@ local patch_to_code = function (in_patch_data)
 		local ggg = Fggg & 7
 		local bbb = Abbb & 7
 		local aaa = Haaa & 7
-
 		table.insert(tempcode, A | aaa)
 		table.insert(tempcode, B | bbb)
 		table.insert(tempcode,     ccc)
@@ -136,7 +106,6 @@ end
 
 local code = string.upper(arg[1])
 local code_minus_one = ''
-local code_test = ''
 local code_plus_one = ''
 local patch_data = code_to_patch(code)
 
